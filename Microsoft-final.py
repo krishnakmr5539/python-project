2fadb714645148a8a7e00fcb800d24d7224c969f
Krishna Kumar <krishnakmr5539@gmail.com>
2024-04-02 03:19:27 -0700

    added time

--- Microsoft-final.py
+++ Microsoft-final.py
@@ -2,18 +2,18 @@ import os
 import shutil
 import tarfile
 import sys
-import time
+# import time
 import datetime
 
-today_date = time.strftime("%Y-%m%d") #time in 2024-0326 formate
-year = time.strftime("%Y")
+# today_date = time.strftime("%Y-%m%d") #time in 2024-0326 formate
+# year = time.strftime("%Y")
 
 # path of the directory 
 master_dir = "/volume/CSdata/krikumar/Microsoft"
 
 # dir_path = "/volume/CSdata/krikumar/Microsoft/" + today_date
 
-print("Todays date:",today_date)
+# print("Todays date:",today_date)
 # Getting the list of directories 
 dir = os.listdir(master_dir) 
 
@@ -24,9 +24,10 @@ for d in dir:
     d1=d
     print("d1:",d1)
     path= "/volume/CSdata/krikumar/Microsoft/" + d1 + "/" + "test"
+    print(path)
     create_time = os.path.getctime(path)
     create_date = datetime.datetime.fromtimestamp(create_time)
-    print("create_time:",create_time)
+    print("create_time:",create_date)
 
 # os.chdir(master_dir) 
 # os.system('ls -l')
